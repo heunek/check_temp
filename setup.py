791820a724b009c8fd8d4508a6594756047c70f3
michael <42749620+heunek@users.noreply.github.com>
2018-10-19 14:47:35 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name = 'check_temp',
+    version = '1.0',
+    description = 'Remote Icinga2 command for checking the ARM core temperature',
+    author = 'heunek',
+    author_email = 'heunek@watership.de',
+    url = 'watership.de,
+    py_modules = ['os'],
